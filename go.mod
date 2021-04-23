e7d5b52b08d2f477c33dffc10923b27d61ed71f1
Mazeorz <mazeorz.me@gmail.com>
2021-04-23 17:09:29 +0800

    build

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module Clash.Tray
 
 go 1.16
 
-require github.com/spf13/viper v1.7.1 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/getlantern/systray v1.1.0
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+)
--- main.go
+++ main.go
@@ -1,48 +1,37 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"github.com/spf13/viper"
-	"log"
+	"C"
+	"Clash.Tray/icon"
+	"github.com/getlantern/systray"
+	"github.com/getlantern/systray/example/icon"
 	"os"
 )
 
-func init() {
-
-	viper.SetConfigName("config") // 读取yaml配置文件
-
-	//viper.AddConfigPath("/etc/appname/")   //设置配置文件的搜索目录
-	//viper.AddConfigPath("$HOME/.appname")  // 设置配置文件的搜索目录
-
-	viper.AddConfigPath(".") // 设置配置文件和可执行二进制文件在用一个目录
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			// Config file not found; ignore error if desired
-			log.Println("no such config file")
-		} else {
-			// Config file was found but another error was produced
-			log.Println("read config error")
-		}
-		log.Fatal(err) // 读取配置文件失败致命错误
-	}
-}
-
 func main() {
-	fmt.Println("获取配置文件的port", viper.GetInt("port"))
-	fmt.Println("获取配置文件的redis", viper.Get(`proxies`))
-	fmt.Println(ReadLine(1))
+	systray.Run(onReady, onExit)
 }
 
-func ReadLine(lineNumber int) string {
-	file, _ := os.Open("config.yaml")
-	fileScanner := bufio.NewScanner(file)
-	lineCount := 1
-	for fileScanner.Scan() {
-		if lineCount == lineNumber {
-			return fileScanner.Text()
+func onReady() {
+	systray.SetIcon(icon.Data2)
+	systray.SetTitle("Clash.Tray")
+	systray.SetTooltip("A Tray tool for Clash")
+	systray.AddMenuItem("Clash.Tray", "")
+	systray.AddSeparator()
+	RuleSwitch := systray.AddMenuItem("Rule", "")
+	RuleSwitch.AddSubMenuItem("Global", "")
+	RuleSwitch.AddSubMenuItem("Rule", "")
+	RuleSwitch.AddSubMenuItem("Direct", "")
+	mQuit := systray.AddMenuItem("Exit", "Quit")
+	go func() {
+		select {
+		case <-mQuit.ClickedCh:
+			systray.Quit()
+			return
 		}
-		lineCount++
-	}
-	return ""
+	}()
+}
+
+func onExit() {
+	os.Exit(1)
 }
